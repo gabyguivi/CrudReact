b7c1828dbfce67ece93f3acac0b8976be426a933
Guivi <gguivisdalsky@accessinformatica.com.ar>
2019-12-04 17:36:23 -0300

    trabajando con estados y eventos

--- src/src/Components/car.js
+++ src/src/Components/car.js
@@ -1,20 +1,41 @@
 import React from "react";
 
 class Car extends React.Component {
-  render() {
-    return <h2>I am a {this.props.color} Car!</h2>;
+  state = {
+    brand: { name: "Renault", model: "Sandero" },
+    color: this.props.favcol,
+    year: 2017
+  };
+  componentDidMount() {
+    console.log(this.props);
   }
-}
 
-class Garage extends React.Component {
+  handleChange = e => {
+    console.log(e.target.value);
+  };
+
+  changeColor = () => {
+    if (this.state.color === "red" || this.state.color === "Fire red")
+      this.setState({ color: "blue" });
+    else this.setState({ color: "red" });
+  };
   render() {
     return (
       <div>
-        <h1>Who lives in my Garage? {this.props.garage}</h1>
-        <Car color={this.props.color} />
+        <div>
+          <input onChange={this.handleChange}></input>
+        </div>
+        <h1>My {this.state.brand.name}</h1>
+        <p>
+          It is a {this.state.color}&nbsp;
+          {this.state.brand.model}&nbsp;from {this.state.year}.
+        </p>
+        <button type="button" onClick={this.changeColor}>
+          Change color
+        </button>
       </div>
     );
   }
 }
 
-export { Car, Garage };
+export { Car };
--- src/src/index.js
+++ src/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Garage } from "./Components/car.js";
+import { Car } from "./Components/car.js";
 
-ReactDOM.render(
-  <Garage color="red" garage="Sandero Fuego" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Car favcol="fire red" />, document.getElementById("root"));
